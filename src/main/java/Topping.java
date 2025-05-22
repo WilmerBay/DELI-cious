bce1f993067152d7b4db3d4fdf50ed36cc0cdb70
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-22 16:06:18 -0700

    taking out enums, simplifying using Strings

--- src/main/java/Topping.java
+++ src/main/java/Topping.java
@@ -1,13 +1,13 @@
 public class Topping {
 
     private String name;
-    private ToppingType type;
+    private String type;
     private boolean isExtra;
 
-    public Topping(String name, ToppingType type, boolean isExtra) {
+    public Topping(String name, String type, boolean isExtra) {
 
         this.name = name;
-        this.type = type;
+        this.type = type.toLowerCase().trim();
         this.isExtra = isExtra;
     }
 
@@ -15,69 +15,75 @@ public class Topping {
 
         double basePrice = 0.0;
 
-        switch (type) {
+        if (type.equals("meat")) {
 
-            case MEAT -> {
-
-                basePrice = switch (size) {
-                    case "4" -> 1.00;
-                    case "8" -> 2.00;
-                    case "12" -> 3.00;
+            basePrice = switch (size) {
+                case "4" -> 1.00;
+                case "8" -> 2.00;
+                case "12" -> 3.00;
+                default -> 0.00;
+            };
+            if (isExtra) {
+                basePrice += switch (size) {
+                    case "4" -> 0.50;
+                    case "8" -> 1.00;
+                    case "12" -> 1.50;
                     default -> 0.00;
                 };
-                if (isExtra) {
-                    basePrice += switch (size) {
-                        case "4" -> 0.50;
-                        case "8" -> 1.00;
-                        case "12" -> 1.50;
-                        default -> 0.00;
-                    };
-                }
             }
-            case CHEESE -> {
-                basePrice = switch (size) {
-                    case "4" -> 0.75;
-                    case "8" -> 1.50;
-                    case "12" -> 2.25;
+        } else if (type.equals("cheese")) {
+            basePrice = switch (size) {
+                case "4" -> 0.75;
+                case "8" -> 1.50;
+                case "12" -> 2.25;
+                default -> 0.00;
+            };
+            if (isExtra) {
+                basePrice += switch (size) {
+                    case "4" -> 0.30;
+                    case "8" -> 0.60;
+                    case "12" -> 0.90;
                     default -> 0.00;
                 };
-                if (isExtra) {
-                    basePrice += switch (size) {
-                        case "4" -> 0.30;
-                        case "8" -> 0.60;
-                        case "12" -> 0.90;
-                        default -> 0.00;
-                    };
-                }
             }
-            case REGULAR, SAUCE -> {
-                basePrice = 0.0; // included in sandwich, if it was an added topping we can use above format depending on size
-            }
+        } else if (type.equals("regular") || type.equals("sauce")) {
+            basePrice = 0.0;  // Always included
+        } else {
+            System.out.println("Warning: Unknown topping type '" + type + "'");
         }
+
         return basePrice;
     }
 
+    // Getters
     public String getName() {
         return name;
     }
 
-    public ToppingType getType() {
+    public String getType() {
         return type;
     }
 
     public boolean isExtra() {
         return isExtra;
     }
+
+    // Setters
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setType(ToppingType type) {
-        this.type = type;
+    public void setType(String type) {
+        this.type = type.toLowerCase().trim();
     }
 
     public void setExtra(boolean isExtra) {
         this.isExtra = isExtra;
+    }
+
+    @Override
+    public String toString() {
+        return name + (isExtra ? " (extra)" : "");
     }
 }
 
--- src/main/java/ToppingType.java
+++
@@ -1,6 +0,0 @@
-public enum ToppingType {
-
-    // shout out to paulo explaining that enums is by default a static class on its own so it uses it's own file.
-
-    MEAT, CHEESE, REGULAR, SAUCE // enums to use for case switch for the get price method
-}
