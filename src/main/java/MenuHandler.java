dae90ae5a5434c6aa214128fe1f89d2c788b229d
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-23 14:35:46 -0700

    menu sandwich builder prompts for user to select their choices

--- src/main/java/MenuHandler.java
+++ src/main/java/MenuHandler.java
@@ -34,7 +34,58 @@ public class MenuHandler { // UI for user's to input
             }
         }
     }
-    private void startSandwichBuilder(){}
+    private void startSandwichBuilder(){
+
+        System.out.println("""
+        --- Build Your Sandwich ---
+        """);
+
+        String size = "";
+
+        while (true) {
+            System.out.println("""
+            Choose a size:
+            1) 4"
+            2) 8"
+            3) 12"
+            """);
+
+            System.out.print("> ");
+            String input = scanner.nextLine().trim();
+
+            switch (input) {
+                case "1" -> size = "4";
+                case "2" -> size = "8";
+                case "3" -> size = "12";
+                default -> System.out.println("Invalid choice. Please try again.");
+            }
+            if (!size.isEmpty()) break;
+        }
+
+        String bread = "";
+
+        while (true) {
+            System.out.println("""
+            Choose bread:
+            1) White
+            2) Wheat
+            3) Rye
+            4) Wrap
+            """);
+
+            System.out.print("> ");
+            String input = scanner.nextLine().trim();
+
+            switch (input) {
+                case "1" -> bread = "white";
+                case "2" -> bread = "wheat";
+                case "3" -> bread = "rye";
+                case "4" -> bread = "wrap";
+                default -> System.out.println("Invalid choice. Please try again 1–4.");
+            }
+            if (!bread.isEmpty()) break;
+        }
+    }
 }
 
 
