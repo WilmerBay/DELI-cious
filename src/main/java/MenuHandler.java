be4837c47e4252cf60bfe69919028755c2b1e5b6
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-23 13:57:58 -0700

    Menu ui Building. print outs for terminal and switch case added to start the sandwich builder. sandwichbuilder method created but no logic

--- src/main/java/MenuHandler.java
+++ src/main/java/MenuHandler.java
@@ -1,2 +1,40 @@
-public class MenuHandler {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MenuHandler { // UI for user's to input
+
+    private final Scanner scanner;
+
+    public MenuHandler() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void displayHomeScreen() {
+
+        while (true) {
+            System.out.println("""
+                    =========================
+                    Welcome to DELI-cious!
+                    =========================
+                    1) New Sandwich
+                    0) Exit
+                    """);
+
+            System.out.print("Please select an option: ");
+            String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "1" -> startSandwichBuilder();
+                case "0" -> {
+                    System.out.println("Goodbye!");
+                    return;
+                }
+                default -> System.out.println("Invalid option. Try again.");
+            }
+        }
+    }
+    private void startSandwichBuilder(){}
 }
+
+
