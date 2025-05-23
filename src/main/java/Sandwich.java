f1adaabf281c83269f4c0f112adbe2f33728012e
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-23 11:08:43 -0700

    continued with get setters fow sadwich class

--- src/main/java/Sandwich.java
+++ src/main/java/Sandwich.java
@@ -16,18 +16,55 @@ public class Sandwich {
         this.isToasted = isToasted;
         this.toppings = new ArrayList<>();
     }
-    public void addTopping (Topping topping) {
+    public void addTopping (Topping topping) { //adding to topping class
+
         toppings.add(topping);
     }
     public double calculatePrice() {
 
         double basePrice = switch (size) {
+
             case "4" -> 5.50;
             case "8" -> 7.00;
             case "12" -> 8.50;
             default -> 0.00;
         };
 
-        //for
+        for (Topping topping : toppings) {
+            basePrice += topping.getPrice(size);
+        }
+        return basePrice;
     }
+    public String getSize() {
+        return size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread.toLowerCase().trim();
+    }
+
+    public void setToasted(boolean isToasted) {
+        this.isToasted = isToasted;
+    }
+
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
+
+
 }
