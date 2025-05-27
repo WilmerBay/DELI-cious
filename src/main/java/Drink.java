3ccc758dbe41cf9c6d2da5cefc6263711dbbb772
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-27 16:34:44 -0700

    Drink Class added

--- src/main/java/Drink.java
+++ src/main/java/Drink.java
@@ -1,2 +1,18 @@
 public class Drink {
+
+    private String name;
+
+    public Drink(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // optional: override toString for clean printouts
+    @Override
+    public String toString() {
+        return name;
+    }
 }
